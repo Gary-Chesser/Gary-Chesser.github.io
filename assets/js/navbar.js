7f6769ae7355af335f9d87e55d57292d31348ad7
Gary <58619873+Gary-Chesser@users.noreply.github.com>
2020-08-22 14:46:11 -0400

    Add files via upload
    Feature list to projects

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,3 +8,27 @@ function openNav(){
     document.getElementById("main").style.marginLeft= "375px";
     document.getElementById("footer").style.marginLeft= "375px";
 }
+function hide(i){
+    var x;
+    switch(i){
+        case 1:
+            x = document.getElementById("pay");
+            break;
+        case 2:
+            x = document.getElementById("account");
+            break;
+        case 3:
+            x = document.getElementById("movie");
+            break;
+        case 4:
+            x = document.getElementById("calc");
+            break;
+        default:
+            break;
+    }
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
